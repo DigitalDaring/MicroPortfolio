f39c5aa16705b4dbf21df4b1ec9d7b233b589196
Edmund Bates <edmund@digitaldaring.com>
2019-10-12 12:13:00 +0200

    Started making micro portfolio

---
+++ src/Navigation.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<style>
+    nav {
+        display: flex;
+        align-items: center;
+        justify-self: flex-end;
+        margin-left: auto;
+    }
+
+    nav a {
+        display: inline-block;
+    }
+</style>
+
+<nav>
+    <a href="#about-me">About Me</a>
+    <a href="#credentials">My Credentials</a>
+    <a href="#adventures">Adventures</a>
+</nav>
